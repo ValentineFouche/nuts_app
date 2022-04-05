f95adbd62cb48fabb19a67b25e82cd88e5e944d5
Valentine Fouche <fouche.valentine@gmail.com>
2022-04-05 12:34:12 +0200

    controllers, models et routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :show] do
+    resources :recommendations, only: [ :new, :create]
+  end
+  resources :recommendations, only: [ :show, :index, :edit, :update] do
+    collection do
+      get :searched
+    end
+  end
 end
