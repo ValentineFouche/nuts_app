7ab0260ef7e328f7a3679234dc43c1f36712cebb
Valentine Fouche <fouche.valentine@gmail.com>
2022-04-05 16:50:56 +0200

    correc seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Recommendation.destroy_all
-MoviesList.destroy_all
 Movie.destroy_all
-List.destroy_all
 User.destroy_all
 
 puts "creating users"
