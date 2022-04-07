980c699715ca7c581f3300df903740db4fc08fa6
Ingo Knorr <ingoknorr@gmail.com>
2022-04-07 12:56:30 +0200

    added links to all pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_04_06_135850) do
+ActiveRecord::Schema.define(version: 2022_04_06_101515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
