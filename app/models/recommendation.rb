0eb560fbba26bd22f87fe6df12c2aee125b2459b
eva lampin <eva.lampin@hotmail.fr>
2022-04-08 17:54:31 +0200

    en train de changer des cards

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,5 +1,5 @@
 class Recommendation < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  # validates :comment, presence: true // bug sur la creation
+  # validates :comment, presence: true
 end
