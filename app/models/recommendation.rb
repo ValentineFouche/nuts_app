bc6856a7b0ee21bf1476275ab6302d4ea050c77c
Valentine Fouche <fouche.valentine@gmail.com>
2022-04-12 14:33:02 +0200

    supprimé les validations de movie uniqueness en attendant

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,6 @@
 class Recommendation < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  # validates :movie, uniqueness: true >> plus de validation ici car pose problème avec l'ajout de films d'un ami
+  # validates :movie, uniqueness: true # >> plus de validation ici car pose problème avec l'ajout de films d'un ami
   # (ils ont le même ID). On contourne autrement.
 end
