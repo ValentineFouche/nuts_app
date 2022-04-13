f861cb5e344b7c9579e667825b7a3d7331eefeaf
Valentine Fouche <fouche.valentine@gmail.com>
2022-04-13 17:53:04 +0200

    correcs à droite à gauche

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -5,13 +5,14 @@ const initStarRating = () => {
   $('#recommendation_feedback_rating').barrating({
     theme: 'css-stars'
   });
-  $('#restaurant_feedback_rating').barrating({
   $('#bookrecom_feedback_rating').barrating({
     theme: 'css-stars'
   });
   $('#podcastrecom_feedback_rating').barrating({
     theme: 'css-stars'
   });
+  $('#restaurant_feedback_rating').barrating({
+    theme: 'css-stars'
+  });
 };
-
 export { initStarRating };
