6a6bf3f049a81296f6f99a92b0f68d6f4c73b2a6
eva lampin <eva.lampin@hotmail.fr>
2022-04-13 10:59:43 +0200

    j'ai juste enleve un commentaire qui me gene pour retourner sur le master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     user_recommendations = Recommendation.where(user_id: current_user.id)
-    @user_friends = user_recommendations.map{ |reco|reco.friend } # prend reco du current user, donc prend uniquement ses amis
+    @user_friends = user_recommendations.map{ |reco|reco.friend }
     # @friends = all_friends.uniq.reject! { |friend| User.where(nickname: friend).first == current_user }
     @movies = current_user.recommendations.map {|reco| reco.movie}
   end
