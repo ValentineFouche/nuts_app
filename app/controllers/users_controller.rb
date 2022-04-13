124ac4c5ea60c4751f3a8fcc296864e6d2d33c88
eva lampin <eva.lampin@hotmail.fr>
2022-04-13 10:19:30 +0200

    on peut voir que la liste de nos amis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    all_friends = Recommendation.all.map{ |reco|reco.friend }
-    @friends = all_friends.uniq.reject! { |friend| User.where(nickname: friend).first == current_user }
+    user_recommendations = Recommendation.where(user_id: current_user.id)
+    @user_friends = user_recommendations.map{ |reco|reco.friend } # prend reco du current user, donc prend uniquement ses amis
+    # @friends = all_friends.uniq.reject! { |friend| User.where(nickname: friend).first == current_user }
     @movies = current_user.recommendations.map {|reco| reco.movie}
   end
 end
